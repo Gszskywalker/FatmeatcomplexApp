950ccab171ad55413d7db37080ee0502067e2e9e
Fatmeat <84301924+Gszskywalker@users.noreply.github.com>
2021-12-31 03:34:21 +0800

    Add files via upload

--- controllers/postController.js
+++ controllers/postController.js
@@ -4,24 +4,26 @@ exports.viewCreateScreen = function(req, res) {
     res.render('create-post')
 }
 
-exports.create = function(req, res) {
+exports.create = async function(req, res) {
     let post = new Post(req.body, req.session.user._id)
-    post.create().then(function(newId) {
+    try {
+        const newId = await post.create()
         req.flash("success", "New post successfully created.")
         req.session.save(() => res.redirect(`/post/${newId}`))
-    }).catch(function(errors) {
+    } catch (errors) {
         errors.forEach(error => req.flash("errors", error))
         req.session.save(() => res.redirect("/create-post"))
-    })
+    }
 }
 
-exports.apiCreate = function(req, res) {
+exports.apiCreate = async function(req, res) {
     let post = new Post(req.body, req.apiUser._id)
-    post.create().then(function(newId) {
+    try {
+        const newId = await post.create()
         res.json("Congrats.")
-    }).catch(function(errors) {
+    } catch (errors) {
         res.json(errors)
-    })
+    }
 }
 
 exports.viewSingle = async function(req, res) {
@@ -47,9 +49,10 @@ exports.viewEditScreen = async function(req, res) {
     }
 }
 
-exports.edit = function(req, res) {
+exports.edit = async function(req, res) {
     let post = new Post(req.body, req.visitorId, req.params.id)
-    post.update().then((status) => {
+    try {
+        const status = await post.update()
         // the post was successfully updated in the database
         // or user did have permission, but there were validation errors
         if (status == "success") {
@@ -66,38 +69,41 @@ exports.edit = function(req, res) {
                 res.redirect(`/post/${req.params.id}/edit`)
             })
         }
-    }).catch(() => {
+    } catch (errors) {
         // a post with the requested id doesn't exist
         // or if the current visitor is not the owner of the requested post
         req.flash("errors", "You do not have permission to perform that action.")
         req.session.save(function() {
             res.redirect("/")
         })
-    })
+    }
 }
 
-exports.delete = function(req, res) {
-    Post.delete(req.params.id, req.visitorId).then(() => {
+exports.delete = async function(req, res) {
+    try {
+        await Post.delete(req.params.id, req.visitorId)
         req.flash("success", "Post successfully deleted.")
         req.session.save(() => res.redirect(`/profile/${req.session.user.username}`))
-    }).catch(() => {
+    } catch (errors) {
         req.flash("errors", "You do not have permission to perform that action.")
         req.session.save(() => res.redirect("/"))
-    })
+    }
 }
 
-exports.apiDelete = function(req, res) {
-    Post.delete(req.params.id, req.apiUser._id).then(() => {
+exports.apiDelete = async function(req, res) {
+    try {
+        await Post.delete(req.params.id, req.apiUser._id)
         res.json("Success")
-    }).catch(() => {
+    } catch (errors) {
         res.json("You do not have permission to perform that action.")
-    })
+    }
 }
 
-exports.search = function(req, res) {
-    Post.search(req.body.searchTerm).then(posts => {
+exports.search = async function(req, res) {
+    try {
+        const posts = await Post.search(req.body.searchTerm)
         res.json(posts)
-    }).catch(() => {
+    } catch (errors) {
         res.json([])
-    })
+    }
 }
